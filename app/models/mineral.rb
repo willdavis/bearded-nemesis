1da62da615db5293390e813b48b84abcf2e614ae
Bill Davis <bdavis84@pacbell.net>
2013-07-12 17:13:21 -0700

    added validations

--- app/models/mineral.rb
+++ app/models/mineral.rb
@@ -1,3 +1,6 @@
 class Mineral < ActiveRecord::Base
   attr_accessible :name, :type_id
+  
+  validates :name,    :presence => true
+  validates :type_id, :presence => true, numericality: { only_integer: true }
 end
--- app/models/moon.rb
+++ app/models/moon.rb
@@ -1,3 +1,7 @@
 class Moon < ActiveRecord::Base
   attr_accessible :celestial_id, :location_id, :name
+  
+  validates :name,          :presence => true
+  validates :celestial_id,  :presence => true, numericality: { only_integer: true }
+  validates :location_id,   :presence => true, numericality: { only_integer: true }
 end
