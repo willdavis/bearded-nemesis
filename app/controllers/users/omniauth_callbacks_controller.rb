e994d8326085a4dde8bc829f0c1c9f442b53804c
Bill Davis <bdavis84@pacbell.net>
2013-07-18 14:00:32 -0700

    signin and redirect to @user

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -8,7 +8,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.find_for_braveid_oauth(env['omniauth.auth'], current_user)
     
     if @user.persisted?
-      sign_in_and_redirect root_path, :event => :authentication #this will throw if @user is not activated
+      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "BraveID") if is_navigational_format?
     else
       session["devise.brave_id_data"] = env['omniauth.auth']
