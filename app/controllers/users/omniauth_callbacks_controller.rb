539dcb4e87a81ba2368868f67adde7416679331a
Bill Davis <bdavis84@pacbell.net>
2013-07-17 23:08:10 -0700

    temporarily removed auth from moons controller.  now display uid if logged in

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,12 +1,15 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def brave_id
-    @user = User.find_for_braveid_oauth(params["omniauth.auth"], current_user)
+    puts "(brave_moonraker) Omniauth callback initiated"
+    puts "(brave_moonraker) Omniauth params: #{env['omniauth.auth']}"
+    
+    @user = User.find_for_braveid_oauth(env['omniauth.auth'], current_user)
     
     if @user.persisted?
       sign_in_and_redirect root_path, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "BraveID") if is_navigational_format?
     else
-      session["devise.brave_id_data"] = params["omniauth.auth"]
+      session["devise.brave_id_data"] = env['omniauth.auth']
       redirect_to new_user_registration_url
     end
   end
