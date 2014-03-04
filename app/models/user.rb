80345916fc7f7870dfb0ba1a36b9623967c4f393
willdavis <bdavis84@pacbell.net>
2014-03-04 12:50:10 -0800

    added role:string column to User table and added User::Roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,8 @@ class User < ActiveRecord::Base
   
   has_many :moons
   
+  Roles = [ :admin , :banned, :scout, :observer, :default ]
+  
   def self.find_for_braveid_oauth(auth, signed_in_resource=nil)
     user = User.where(:provider => auth.provider, :uid => auth.uid.to_s).first
     unless user
@@ -22,5 +24,9 @@ class User < ActiveRecord::Base
         )
     end
     user
+  end
+
+  def is?(requested_role)
+    self.role == requested_role.to_s
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130721223507) do
+ActiveRecord::Schema.define(:version => 20140304204242) do
 
   create_table "minerals", :force => true do |t|
     t.integer  "type_id"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(:version => 20130721223507) do
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.string   "name"
+    t.string   "role"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
